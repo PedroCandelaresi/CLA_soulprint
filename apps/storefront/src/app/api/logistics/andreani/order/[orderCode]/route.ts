afc3e720ab08903f4de9798040444b08d37b8c3f
Pedro Candelaresi <pcandelaresi.nqn@gmail.com>
2026-03-27 14:09:47 -0300

    123

--- apps/storefront/src/app/api/logistics/andreani/order/[orderCode]/route.ts
+++ apps/storefront/src/app/api/logistics/andreani/order/[orderCode]/route.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { appendVendureSetCookieHeaders } from '@/lib/vendure/client';
-import { buildAndreaniBackendUrl } from '../utils';
+import { buildAndreaniBackendUrl } from '../../utils';
 
 export const dynamic = 'force-dynamic';
 
